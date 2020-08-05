205c7463a259936d84d9929a5c66df7793a7debc
IgorBezusenko <bezusenko.98@mail.ru>
2020-08-05 16:14:20 +0300

    Added new App react-todo

---
+++ build/precache-manifest.a89482fe653bd43bb081946a12e3ec9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26552f13cd2aeed5f760fb27e4af1826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "506a06c0a911df463366",
+    "url": "/static/css/main.09576eb3.chunk.css"
+  },
+  {
+    "revision": "517eba60d80a0ae1a83e",
+    "url": "/static/js/2.151afbce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.151afbce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506a06c0a911df463366",
+    "url": "/static/js/main.401dc8a8.chunk.js"
+  },
+  {
+    "revision": "5797d46da30f2a2273be",
+    "url": "/static/js/runtime-main.3bf9f130.js"
+  }
+]);
